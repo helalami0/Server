b68795e346584e922930dced361210ee098f956c
Hachem El Alami <helalami0@gmail.com>
2021-12-28 16:01:54 +0100

    ready to deploy

---
+++ testing.js
@@ -0,0 +1,18 @@
+user = []
+
+user.push({
+    userID: "userID",
+    myShipsLocation: "0001",
+    foundShips: 0
+})
+
+user.push({
+    userID: "userID2",
+    myShipsLocation: "0100",
+    foundShips: 2
+})
+
+for(const x of user){
+    console.log(x)
+
+}
