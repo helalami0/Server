b60b046a6c4aeefa4a2a2de9cdc8bf8db119d46f
Hachem El Alami <helalami0@gmail.com>
2021-12-30 02:19:45 +0100

    Commit V1.01

--- db.js
+++ db.js
@@ -5,19 +5,19 @@ const pg = require("pg");
 const {Pool} = pg;
 
 let localPoolConfig = {
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    database:process.env.DB_DATABASE
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
 }
-console.log("test: ", process.env.DATABASE_URL)
-const poolConfig = process.env.DATABASE_URL? {
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-} : localPoolConfig;
 
-const pool = new Pool(poolConfig);
+// const poolConfig = process.env.DATABASE_URL? {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: { rejectUnauthorized: false }
+// } : localPoolConfig;
+
+const pool = new Pool(localPoolConfig);
 
 module.exports = {
     getPool: function () {
